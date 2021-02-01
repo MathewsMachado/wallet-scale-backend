ea5355c783bd125913752c06997eca2e592c1dc9
Mathews Machado <mathews.machadoamorim@gmail.com>
2021-02-01 19:45:53 -0300

    feat: add class that connects the project with database

---
+++ src/database/Database.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const { Client } = require('pg');
+
+const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
+
+class Database {
+  constructor() {
+    this.client = new Client({
+      host: DB_HOST,
+      port: DB_PORT,
+      user: DB_USER,
+      password: DB_PASSWORD,
+      database: DB_DATABASE,
+    });
+
+    this.client.connect();
+  }
+}
+
+module.exports = new Database();
