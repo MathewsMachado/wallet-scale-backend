ed084bbfd14f2f665d502ae15564e20af3fd7cc7
Mathews Machado <mathews.machadoamorim@gmail.com>
2021-02-01 13:52:48 -0300

    feat: add class GenerateTransactions and mockup that together will populate the database

---
+++ src/app/utils/GenerateTransactions.js
@@ -0,0 +1,64 @@
+const { v4 } = require('uuid');
+
+class GenerateTransactions {
+  generate(mockup) {
+    const currentYear = new Date().getFullYear();
+    const years = [currentYear - 1, currentYear, currentYear + 1];
+    const months = Array.from({ length: 12 }, (_, index) => index + 1);
+    const daysInAMonth = 30;
+    const transactions = [];
+
+    mockup.forEach(({ category, descriptions }) => (
+      years.forEach((year) => (
+        months.forEach((month) => {
+          for (let day = 1; day < daysInAMonth; day += 1) {
+            if (month === 2 && day > 28) return;
+
+            if (this.isTransactionValid()) {
+              transactions.push({
+                category,
+                id: v4(),
+                description: this.getRandomDescription(descriptions),
+                type: category === 'Income' ? '+' : '-',
+                value: this.generateRandomValue(),
+                date: this.formatDate(year, month, day),
+              });
+            }
+          }
+        })
+      ))
+    ));
+
+    return transactions;
+  }
+
+  getRandomDescription(descriptions) {
+    const max = descriptions.length;
+    const randomIndex = Math.floor(Math.random() * max);
+
+    return descriptions[randomIndex];
+  }
+
+  generateRandomValue() {
+    const randomValue = new Date().getMilliseconds() * Math.random();
+    return randomValue.toFixed(2);
+  }
+
+  isTransactionValid() {
+    const randomBoolean = Boolean(
+      Math.round(Math.random()),
+    );
+
+    return randomBoolean && randomBoolean;
+  }
+
+  fillDateChunk(dateChunk) {
+    return dateChunk.toString().padStart(2, '0');
+  }
+
+  formatDate(year, month, day) {
+    return `${year}/${this.fillDateChunk(month)}/${this.fillDateChunk(day)}`;
+  }
+}
+
+module.exports = new GenerateTransactions().generate;
