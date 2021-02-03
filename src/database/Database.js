2f6132a88d1851f687d078f6636f81333f781674
Mathews Machado <mathews.machadoamorim@gmail.com>
2021-02-02 22:14:16 -0300

    feat: add populateTransactionsTable method to Database class

--- src/database/Database.js
+++ src/database/Database.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 
 const { Client } = require('pg');
 
+const mockup = require('./mockup.js');
+
+const GenerateTransactions = require('../app/utils/GenerateTransactions.js');
+
 const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
 
 class Database {
@@ -15,12 +19,33 @@ class Database {
     });
 
     this.client.connect();
+    this.populateTransactionsTable();
   }
 
   async query(statement, values) {
     const { rows } = await this.client.query(statement, values);
 
     return rows;
+  }
+
+  async populateTransactionsTable() {
+    const [isTableFilled] = await this.query('SELECT * FROM transactions LIMIT 1', []);
+
+    if (isTableFilled) {
+      return;
+    }
+
+    const transactions = GenerateTransactions.generate(mockup);
+
+    transactions.forEach(async ({ id, category, description, type, value, date }) => {
+      await this.query(
+        `INSERT INTO transactions(id, category, description, type, value, date)
+        VALUES($1, $2, $3, $4, $5, $6)`,
+        [id, category, description, type, value, date],
+      );
+    });
+
+    console.log('Database was successfully populated');
   }
 }
 
